6cd8f88bf2d3f73109ee40eef525765b89865d7f
VVSD Charan <105978561+VVSD-Charan@users.noreply.github.com>
2023-07-29 17:21:07 +0530

    Time: 419 ms (39.83%) | Memory: 15.8 MB (33.67%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList)
     {
-        set<string>s(wordList.begin(),wordList.end());
-        if(beginWord == endWord)return 0;
-        if(s.find(endWord)==s.end())return 0;
+        set<string>s;
+        for(auto it : wordList)s.insert(it);
 
-        queue<string>q;
-        q.push(beginWord);
-
+        if(s.find(endWord)==s.end() || beginWord==endWord)return 0;
+        if(beginWord.length()!=endWord.length())return 0;
         if(s.find(beginWord)!=s.end())s.erase(s.find(beginWord));
 
+        queue<string>q;   q.push(beginWord);
         int changes = 0;
 
-        while(q.size() && s.size())
+        while(q.size()>0 && s.size()>0)
         {
             int nodes = q.size();
             changes++;
@@ -25,18 +24,17 @@ public:
                 for(int i=0;i<str.length();i++)
                 {
                     char initial = str[i];
-                    for(char j ='a';j<='z';j++)
+                    for(char j='a';j<='z';j++)
                     {
                         str[i] = j;
+                        if(str == endWord)return changes+1;
                         if(s.find(str)!=s.end())
                         {
                             s.erase(s.find(str));
                             q.push(str);
-
-                            if(str == endWord)return changes+1;
                         }
                     }
-                    str[i]=initial;
+                    str[i] = initial;
                 }
             }
         }
