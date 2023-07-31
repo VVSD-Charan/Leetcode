75f8dd72a711acd0c86b663eed6a06d754f8215e
VVSD Charan <105978561+VVSD-Charan@users.noreply.github.com>
2023-07-31 09:52:58 +0530

    Time: 11 ms (98.89%) | Memory: 17.9 MB (38.24%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -5,10 +5,11 @@ public:
         int n = nums.size();
         int required_sum = (n*(n+1))/2;
 
-        int current_sum = accumulate(nums.begin(),nums.end(),0);
+        for(auto it : nums)
+        {
+            required_sum -= it;
+        }
 
-        if(current_sum == required_sum)return 0;
-
-        return required_sum - current_sum;
+        return required_sum;
     }
 };
