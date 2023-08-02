dca8e86922ed8724b4ed6c69d350cdaf562c7a80
VVSD Charan <105978561+VVSD-Charan@users.noreply.github.com>
2023-08-02 15:34:46 +0530

    Time: 41 ms (90.24%) | Memory: 18.9 MB (37.63%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+
+    bool possible(vector<int>&piles,int n,int h)
+    {
+        int sum = 0;
+
+        for(int i=0;i<piles.size();i++)
+        {
+            sum += (piles[i]/n) + (piles[i]%n==0? 0:1);
+
+            if(sum > h)return false;
+        }
+
+        return (sum <= h);
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h)
+    {
+        int lo = 1;
+        int hi = 1e9;
+
+        while(hi - lo > 1)
+        {
+            int mid = (lo + hi)>>1;
+
+            if(possible(piles,mid,h))
+            {
+                hi = mid;
+            }
+            else
+            {
+                lo = mid;
+            }
+        }
+
+        if(possible(piles,lo,h))return lo;
+        return hi;    
+    }
+};
