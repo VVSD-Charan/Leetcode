b7e320144227b3d88883a5a8dbfa615a8a2c747a
VVSD Charan <105978561+VVSD-Charan@users.noreply.github.com>
2023-08-16 16:01:20 +0530

    Time: 753 ms (5.04%) | Memory: 121.6 MB (5.02%) - LeetSync

---
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int minimumEffortPath(vector<vector<int>>& heights) 
+    {
+        int n = heights.size();
+        int m = heights[0].size();
+
+        int dx[4] = {-1,+1,0,0};
+        int dy[4] = {0,0,-1,+1};
+
+        vector<vector<int>>distance(n,vector<int>(m,1e7));    
+        distance[0][0] = 0;
+
+        queue<pair<int,pair<int,int>>>q;
+        q.push({0,{0,0}});
+
+        while(q.size() > 0)
+        {
+            auto it = q.front();   q.pop();
+            int dist = it.first;
+            int x = it.second.first;
+            int y = it.second.second;
+
+            for(int i = 0 ; i < 4 ; i++)
+            {
+                int cx = x + dx[i];
+                int cy = y + dy[i];
+
+                if(cx >=0 && cy>=0 && cx < n && cy < m )
+                {
+                    int diff = max(dist , abs(heights[cx][cy] - heights[x][y]));
+
+                    if(distance[cx][cy] > diff)
+                    {
+                        distance[cx][cy] = diff;
+                        q.push({distance[cx][cy],{cx,cy}});
+                    }
+                }
+            }
+        }
+
+        return distance[n-1][m-1];
+    }
+};
