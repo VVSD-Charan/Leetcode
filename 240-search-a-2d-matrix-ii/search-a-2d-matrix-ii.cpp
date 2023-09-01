91df4273378cc44357efd341325c783ea824c1a7
VVSD Charan <105978561+VVSD-Charan@users.noreply.github.com>
2023-09-01 14:50:12 +0530

    Time: 118 ms (39.74%) | Memory: 15 MB (15.11%) - LeetSync

---
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) 
+    {
+        int row = matrix.size() - 1;
+        int col = 0;
+        
+        while(row >= 0 && col < matrix[row].size())
+        {
+            if(matrix[row][col] == target)return true;
+
+            if(matrix[row][col] > target)
+            {
+                row--;
+            }
+            else
+            {
+                col++;
+            }
+        }
+
+        return false;
+    }
+};
