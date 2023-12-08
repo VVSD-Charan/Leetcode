ee0f2d37aa6c9d2f1aacf3e4d27ee26937436899
VVSD Charan <105978561+VVSD-Charan@users.noreply.github.com>
2023-12-08 22:59:11 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (13.01%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -8,45 +8,55 @@ public:
         
     }
     
-    void push(int x) {
-        s1.push(x);
+    void push(int x) 
+    {
+        s1.push(x);    
     }
     
-    int pop() {
-        if(s1.size() == 0)return -1;
+    int pop() 
+    {
+        int lastElement = -1;
 
-        while(s1.size() != 1){
-            s2.push(s1.top());  s1.pop();
+        while(s1.size() > 1)
+        {
+            s2.push(s1.top());
+            s1.pop();
+        }    
+
+        lastElement = s1.top(); s1.pop();
+
+        while(s2.size())
+        {
+            s1.push(s2.top());
+            s2.pop();
         }
 
-        int number = s1.top();  s1.pop();
-
-        while(s2.size() > 0){
-            s1.push(s2.top());  s2.pop();
-        }
-
-        return number;
+        return lastElement;
     }
     
-    int peek() {
-        if(s1.size() == 0)return -1;
+    int peek() 
+    {
+        int lastElement = -1;
 
-        int number;
+        while(s1.size())
+        {
+            s2.push(s1.top());
+            lastElement = s1.top();
+            s1.pop();
+        }    
 
-        while(s1.size() > 0){
-            number = s1.top();
-            s2.push(s1.top());  s1.pop();
+        while(s2.size())
+        {
+            s1.push(s2.top());
+            s2.pop();
         }
 
-        while(s2.size() > 0){
-            s1.push(s2.top());  s2.pop(); 
-        }
-
-        return number;
+        return lastElement;
     }
     
-    bool empty() {
-        return (s1.size() == 0);
+    bool empty() 
+    {
+        return (s1.size() == 0);    
     }
 };
 
