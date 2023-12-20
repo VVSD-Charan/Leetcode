f9c421a13d97661aecef400ce19cfab04a127bea
VVSD Charan <105978561+VVSD-Charan@users.noreply.github.com>
2023-12-20 07:36:28 +0530

    Time: 14 ms (48.64%) | Memory: 47 MB (58.30%) - LeetSync

---
+++ 2756-buy-two-chocolates/buy-two-chocolates.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) 
+    {
+        int first_min = INT_MAX;
+        int second_min = INT_MAX;
+
+        for(int i = 0 ; i < prices.size() ; i++)
+        {
+            if(prices[i] <= first_min)
+            {
+                second_min = first_min;
+                first_min = prices[i];
+            }
+            else if(prices[i] < second_min)
+            {
+                second_min = prices[i];
+            }
+        }
+
+        if(first_min + second_min > money)
+        {
+            return money;
+        }    
+
+        return money-first_min-second_min;
+    }
+};
