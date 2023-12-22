48a451eab291a9c89ab15322c441611cc8a114ed
VVSD Charan <105978561+VVSD-Charan@users.noreply.github.com>
2023-12-22 18:58:46 +0530

    Time: 26 ms (33.85%) | Memory: 15.8 MB (82.00%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,22 +1,22 @@
-class DSU 
+class DisjointSet
 {
-    public :
-
     vector<int>parent;
     vector<int>csize;
 
-    DSU(int n)
+    public:
+
+    DisjointSet(int n)
     {
         parent.resize(n);
         csize.resize(n,1);
 
-        for(int i=0;i<n;i++)parent[i]=i;
+        for(int i = 0 ; i < n ; i++)parent[i]=i;
     }
 
-    int findParent(int u)
+    int findParent(int n)
     {
-        if(parent[u]==u)return u;
-        return parent[u]=findParent(parent[u]);
+        if(parent[n] == n)return n;
+        return parent[n] = findParent(parent[n]);
     }
 
     void Union(int u,int v)
@@ -24,45 +24,47 @@ class DSU
         int pu = findParent(u);
         int pv = findParent(v);
 
-        if(pu==pv)return;
-        if(csize[pu]>csize[pv])
+        if(pu == pv)return;
+
+        if(csize[pu] >= csize[pv])
         {
-            parent[pv]=pu;
-            csize[pu]+=csize[pv];
+            csize[pu] += csize[pv];
+            parent[pv] = pu;
         }
         else
         {
-            parent[pu]=pv;
-            csize[pv]+=csize[pu];
+            csize[pv] += csize[pu];
+            parent[pu] = pv;
         }
     }
 };
 
 class Solution {
 public:
-    int findCircleNum(vector<vector<int>>& isConnected)
+    int findCircleNum(vector<vector<int>>& isConnected) 
     {
         int n = isConnected.size();
-        DSU ds(n+1);
 
-        for(int i=0;i<n;i++)
+        DisjointSet DS(n);
+
+        for(int i = 0 ; i < n ; i++)
         {
-            for(int j=i+1;j<n;j++)
+            for(int j = i+1 ; j < n ; j++)
             {
-                if(isConnected[i][j]==1)
+                if(isConnected[i][j] == 1)
                 {
-                    ds.Union(i,j);
+                    DS.Union(i,j);
                 }
             }
         }
 
-        int cnt = 0;
+        int components = 0;
 
-        for(int i=0;i<n;i++)
+        for(int i = 0 ; i < n ; i++)
         {
-            if(ds.findParent(i)==i)cnt++;
-        }
+            components += (DS.findParent(i) == i);
+        }    
 
-        return cnt;
+        return components;
     }
 };
