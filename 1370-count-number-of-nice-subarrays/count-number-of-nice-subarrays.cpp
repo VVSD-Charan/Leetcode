f3662246d94077dd3d9a077cb4c0c88e4a826cc9
VVSD Charan <105978561+VVSD-Charan@users.noreply.github.com>
2023-12-26 10:06:21 +0530

    Time: 132 ms (39.43%) | Memory: 67.9 MB (92.74%) - LeetSync

---
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -0,0 +1,35 @@
+class Solution {
+
+    int atMost(vector<int>&arr,int k)
+    {
+        int total_arrays = 0;
+        int total_odd = 0;
+        int lo = 0;
+        int hi = 0;
+
+        while(hi < arr.size())
+        {
+            total_odd += (arr[hi]%2);
+
+            while(lo < hi && total_odd > k)
+            {
+                total_odd -= (arr[lo]%2);
+                lo++;
+            }
+
+            if(total_odd <= k)
+            {
+                total_arrays += (hi-lo+1);
+            }
+            hi++;
+        }
+
+        return total_arrays;
+    }
+
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) 
+    {
+        return (atMost(nums,k)-atMost(nums,k-1));    
+    }
+};
