165282c08fd475bcf5daa546eeb1950be7a6e28d
VVSD Charan <105978561+VVSD-Charan@users.noreply.github.com>
2023-12-27 10:00:33 +0530

    Time: 98 ms (22.41%) | Memory: 44.1 MB (53.27%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -2,24 +2,27 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) 
     {
-        int counter = 0;
         int curr_sum = 0;
-
         map<int,int>mp;
 
-        for(auto it : nums)
+        int total_arrays = 0;
+
+        for(int i = 0 ; i < nums.size() ; i++)
         {
-            curr_sum += it;
+            curr_sum = curr_sum + nums[i];
 
             if(curr_sum == k)
             {
-                counter++;
+                total_arrays++;
             }
-            counter+=mp[curr_sum-k];
-            
+
+            if(mp.find(curr_sum - k) != mp.end())
+            {
+                total_arrays += mp[curr_sum-k];
+            }
             mp[curr_sum]++;
         }
 
-        return counter;    
+        return total_arrays;    
     }
 };
