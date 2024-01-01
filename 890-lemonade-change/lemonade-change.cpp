e23802cd811bef907529123d188de38cab202de9
VVSD Charan <105978561+VVSD-Charan@users.noreply.github.com>
2024-01-01 22:24:16 +0530

    Time: 206 ms (5.03%) | Memory: 83.8 MB (57.52%) - LeetSync

--- 890-lemonade-change/lemonade-change.cpp
+++ 890-lemonade-change/lemonade-change.cpp
@@ -9,25 +9,25 @@ public:
         {
             if(bills[i] == 5)
             {
-                count_5++;
+                count_5 = count_5 + 1;
             }
             else if(bills[i] == 10)
             {
                 if(count_5 == 0)return false;
 
-                count_5--;
-                count_10++;
+                count_5 = count_5 - 1;
+                count_10 = count_10 + 1;
             }
             else
             {
                 if(count_10 > 0 && count_5 > 0)
                 {
-                    count_10--;
-                    count_5--;
+                    count_10 = count_10 - 1;
+                    count_5 = count_5 - 1;
                 }
                 else if(count_5 > 2)
                 {
-                    count_5 -= 3;
+                    count_5 = count_5 - 3;
                 }
                 else
                 {
