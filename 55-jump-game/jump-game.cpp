edee673e5a0dba235beb14df1c25be86688b0a73
VVSD Charan <105978561+VVSD-Charan@users.noreply.github.com>
2024-01-02 13:35:13 +0530

    Time: 151 ms (20.49%) | Memory: 48.7 MB (63.69%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -2,21 +2,18 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) 
     {
-        int max_reach = 0;
-        int curr_reach = 0;
+        int maximum_step = 0;
+        int curr_max = 0;
 
-        for(int i = 0 ; i < nums.size()-1 ; i++)
+        for(int i = 0 ; i < nums.size() ; i++)
         {
-            max_reach = max(max_reach,i+nums[i]);
+            curr_max = max(curr_max,i + nums[i]);
 
-            if(max_reach == i)
+            if(maximum_step == i)
             {
-                return false;
-            }
+                if(curr_max <= i && curr_max < nums.size()-1)return false;
 
-            if(curr_reach == i)
-            {
-                curr_reach = max_reach;
+                maximum_step = curr_max;
             }
         }
 
