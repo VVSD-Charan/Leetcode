5dbece8555d1abdcfa598eb2d242fa5246a015f9
VVSD Charan <105978561+VVSD-Charan@users.noreply.github.com>
2024-01-28 07:02:05 +0530

    Time: 489 ms (80.84%) | Memory: 164.4 MB (43.00%) - LeetSync

--- 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.cpp
+++ 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.cpp
@@ -1,10 +1,9 @@
 class Solution {
 
-    int numberTarget(vector<int>&row,int target)
+    int getAll(vector<int>&row,int target)
     {
-        int curr_sum = 0;
+       int curr_sum = 0;
         int total_arrays = 0;
-        int max_len = 0;
 
         map<int,int>mp;
 
@@ -27,33 +26,11 @@ class Solution {
         return total_arrays;
     }
 
-    int col_wise(vector<vector<int>>&matrix,int r,int c,int target)
+    int row_wise(vector<vector<int>>&matrix,int target,int r,int c)
     {
+        int total_matrices = 0;
 
-         int total_matrices = 0;
-         for(int left_col = 0 ;  left_col < c ; left_col++)
-        {
-            vector<int>row_sum(r,0);
-
-            for(int right_col = left_col ; right_col < c ; right_col++)
-            {
-                for(int i = 0 ; i < r ; i++)
-                {
-                    row_sum[i] = row_sum[i] + matrix[i][right_col];
-                }
-
-                total_matrices = total_matrices + numberTarget(row_sum,target);
-            }
-        }
-
-        return total_matrices;
-    }
-
-     int row_wise(vector<vector<int>>&matrix,int r,int c,int target)
-    {
-
-         int total_matrices = 0;
-         for(int first_row = 0 ;  first_row < r ; first_row++)
+        for(int first_row = 0 ; first_row < r ; first_row++)
         {
             vector<int>col_sum(c,0);
 
@@ -61,10 +38,32 @@ class Solution {
             {
                 for(int i = 0 ; i < c ; i++)
                 {
-                    col_sum[i] = col_sum[i] + matrix[last_row][i];
+                    col_sum[i] += matrix[last_row][i];
                 }
 
-                total_matrices = total_matrices + numberTarget(col_sum,target);
+                total_matrices += getAll(col_sum,target);
+            }
+        }
+
+        return total_matrices;
+    }
+
+    int column_wise(vector<vector<int>>&matrix,int target,int r,int c)
+    {
+        int total_matrices = 0;
+
+        for(int first_col = 0 ; first_col < c ; first_col++)
+        {
+            vector<int>row_sum(r,0);
+
+            for(int last_col = first_col ; last_col < c ; last_col++)
+            {
+                for(int i = 0 ; i < r ; i++)
+                {
+                    row_sum[i] += matrix[i][last_col];
+                }
+
+                total_matrices += getAll(row_sum,target);
             }
         }
 
@@ -76,11 +75,12 @@ public:
     {
         int r = matrix.size();
         int c = matrix[0].size();
-        
+
         if(r > c)
         {
-            return col_wise(matrix,r,c,target);
+            return column_wise(matrix,target,r,c);
         }
-        return row_wise(matrix,r,c,target);
+
+        return row_wise(matrix,target,r,c);
     }
 };
