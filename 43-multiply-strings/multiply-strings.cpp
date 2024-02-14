7d1926ca906b45e8e434cba841c6c349b33f6831
VVSD Charan <105978561+VVSD-Charan@users.noreply.github.com>
2024-02-14 22:38:13 +0530

    Time: 14 ms (21.57%) | Memory: 16.5 MB (13.18%) - LeetSync

--- 43-multiply-strings/multiply-strings.cpp
+++ 43-multiply-strings/multiply-strings.cpp
@@ -1,73 +1,72 @@
 class Solution {
+
+    void getProduct(char ch,string &s,vector<int>&temp)
+    {
+        int carry = 0;
+
+        for(int i = s.length()-1 ; i >= 0 ; i--)
+        {
+            int product = (s[i] - '0')*(ch - '0') + carry;
+            
+            temp.push_back(product % 10);
+            carry = (product / 10);
+        }
+
+        if(carry > 0)temp.push_back(carry);
+    }
+
 public:
     string multiply(string num1, string num2) 
     {
-        if(num1 == "0" || num2 == "0")return "0";
+        if(num2.length() > num1.length())return multiply(num2,num1);
 
-        vector<string>products;
-
-        int max_length = 0;
-        int curr = 0;
-
-        for(int i = num1.length()-1 ; i >= 0 ; i-- )
+        //Get individual products
+        vector<vector<int>>arr;
+        int zeroes = num2.length()-1;
+        for(int i = 0 ; i < num2.length() ; i++)
         {
-            string product = "";
-            int remainder = 0;
-
-            for(int j = num2.length()-1 ; j >= 0 ; j--)
-            {
-                int val = (num1[i]-'0')*(num2[j]-'0') + remainder;
-
-                remainder = (val / 10);
-                val = (val % 10);
-
-                product.push_back(val + '0');
+            if(num2[i] == '0'){
+                zeroes = zeroes - 1;
+                continue;
             }
 
-            if(remainder > 0)
+            vector<int>temp;
+            for(int j = 1 ; j <= zeroes ; j++)temp.push_back(0);
+            getProduct(num2[i],num1,temp);
+            arr.push_back(temp);
+
+            zeroes = zeroes - 1;
+        }    
+
+        // Store sum of individual products into string
+        string res = "";
+        int index = 0;
+        int carry = 0;
+        bool done = false;
+
+        while(!done)
+        {
+            int counter = 0;
+            int sum = carry;
+            for(int i = 0 ; i < arr.size() ; i++)
             {
-                product.push_back(remainder + '0');
+                if(index >= arr[i].size())continue;
+                counter = counter + 1;
+                sum = sum + arr[i][index];
             }
 
-             reverse(product.begin(),product.end());
-
-            for(int j = 0 ; j < curr ; j++)
-            {
-                product.push_back('0');
+            if(counter == 0)done = true;
+            else{
+                res.push_back((sum % 10) + '0');
+                carry = (sum / 10);
+                index++;
             }
-
-            reverse(product.begin(),product.end());
-
-            max_length = max(max_length , (int)product.length());
-
-            products.push_back(product);
-            curr++;
         }
 
-        string res_product = "";
-        int reminder = 0;
+        if(carry > 0)res.push_back(carry+'0');
 
-        for(int i = 0 ; i < max_length ; i++)
-        {
-            int sum = reminder;
-
-            for(int j = 0 ; j < products.size() ; j++)
-            {
-                if(i >= products[j].length())continue;
-
-                sum += (products[j][i] - '0');
-            }
-
-            reminder = (sum / 10);
-            sum = (sum % 10);
-
-            res_product.push_back(sum + '0');
-        }
-
-        if(reminder > 0)res_product.push_back(reminder + '0');
-
-        reverse(res_product.begin(),res_product.end());
-
-        return res_product;
+        if(res.length() == 0)return "0";
+        reverse(res.begin(),res.end());
+        return res;
     }
 };
