fa8183e9331313151077f3b781cac3b29cb7bf0e
VVSD Charan <105978561+VVSD-Charan@users.noreply.github.com>
2024-02-15 13:50:04 +0530

    Time: 28 ms (50.27%) | Memory: 94.4 MB (31.09%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -2,47 +2,44 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) 
     {
-        int n1 = nums1.size();
-        int n2 = nums2.size();
+        if(nums1.size() > nums2.size())return findMedianSortedArrays(nums2,nums1);
 
-        if(n1 > n2)return findMedianSortedArrays(nums2,nums1);
+        int n = nums1.size();
+        int m = nums2.size();
 
+        int required = (n + m + 1)/2;
         int lo = 0;
-        int hi = n1;
+        int hi = n;
 
         while(lo <= hi)
         {
-            int mid1 = (lo + hi)/2;
-            int mid2 = ((n1 + n2 + 1)/2) - mid1;
+            int n1 = (lo + hi)>>1;
+            int n2 = required - n1;
 
-            int l1 = (mid1 > 0 ? nums1[mid1-1] : -1e7);
-            int l2 = (mid2 > 0 ? nums2[mid2-1] : -1e7);
-            int r1 = (mid1 < n1 ? nums1[mid1] : 1e7);
-            int r2 = (mid2 < n2 ? nums2[mid2] : 1e7);
+            int l1 = n1 > 0 ? nums1[n1-1] : INT_MIN;
+            int l2 = n2 > 0 ? nums2[n2-1] : INT_MIN;
+            int r1 = (n1 < n && n1 >= 0) ? nums1[n1] : INT_MAX;
+            int r2 = (n2 < m && n2 >= 0) ? nums2[n2] : INT_MAX;
 
             if(l1 <= r2 && l2 <= r1)
             {
-                if(((n1 + n2)%2) == 0)
+                if(((n + m) % 2) == 0)
                 {
-                    double sum = max(l1,l2) + min(r1,r2);
-                    return (sum / 2.0);
+                    double median = ((double)max(l1,l2) + (double)min(r1,r2))/2.0;
+                    return median;
                 }
-                else
-                {
-                    double sum = max(l1,l2);
-                    return sum;
-                }
+                return (double)max(l1,l2);
             }
             else if(l1 > r2)
             {
-                hi = mid1-1;
+                hi = n1-1;
             }
             else
             {
-                lo = mid1+1;
+                lo = n1+1;
             }
         }
 
-        return 0.0;    
+        return (double)0;
     }
 };
