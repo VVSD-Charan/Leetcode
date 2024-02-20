3a68006365938b2e2ca45200227492147f927ae5
VVSD Charan <105978561+VVSD-Charan@users.noreply.github.com>
2024-02-20 12:11:27 +0530

    Time: 1663 ms (74.77%) | Memory: 0B (100.00%) - LeetSync

--- 577-employee-bonus/employee-bonus.sql
+++ 577-employee-bonus/employee-bonus.sql
@@ -1,5 +1,8 @@
 # Write your MySQL query statement below
 
-SELECT e.name,b.bonus
-FROM Employee e LEFT JOIN Bonus as b ON e.empId = b.empId
-WHERE b.bonus is NULL OR b.bonus < 1000; 
+-- SELECT e.name,b.bonus
+-- FROM Employee e LEFT JOIN Bonus as b ON e.empId = b.empId
+-- WHERE b.bonus is NULL OR b.bonus < 1000; 
+
+SELECT e.name , b.bonus FROM Employee e LEFT JOIN Bonus b ON e.empId = b.empId
+WHERE COALESCE(b.bonus,0) < 1000
