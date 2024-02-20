03a2f61f36c9babc8ad870b3766801449bc6be60
VVSD Charan <105978561+VVSD-Charan@users.noreply.github.com>
2024-02-20 08:49:05 +0530

    Time: 16 ms (47.45%) | Memory: 20.4 MB (18.88%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& nums)
+    int missingNumber(vector<int>& nums) 
     {
         int n = nums.size();
-        int required_sum = (n*(n+1))/2;
+        int required_sum =  (n * (n + 1))>>1;
 
         for(auto it : nums)
         {
             required_sum -= it;
-        }
+        }   
 
         return required_sum;
     }
