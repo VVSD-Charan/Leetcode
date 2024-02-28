020382834c046d8c422ba6e6c5bfd14ed2f76641
VVSD Charan <105978561+VVSD-Charan@users.noreply.github.com>
2024-02-28 23:08:03 +0530

    Time: 276 ms (78.19%) | Memory: 145.5 MB (58.77%) - LeetSync

--- 493-reverse-pairs/reverse-pairs.cpp
+++ 493-reverse-pairs/reverse-pairs.cpp
@@ -1,72 +1,85 @@
 class Solution {
 
-    int pairs;
-
-    void mergeArrays(vector<int>&arr,int lo,int mid,int hi)
+    void mergeArrays(vector<pair<int,int>>&arr,int lo,int mid,int hi)
     {
-       int *a = new int[hi - lo + 1];
+        vector<pair<int,int>>temp(hi-lo+1);
+        int i = lo;
+        int j = mid + 1;
+        int index = 0;
 
-       int i = lo;
-       int j = mid + 1;
-       int idx = 0;
-
-       while(i <= mid && j <= hi)
-       {
-           if(arr[i] > arr[j])
-           {
-               a[idx++] = arr[j++];
-           }
-           else
-           {
-               a[idx++] = arr[i++];
-           }
-       }
-
-       while(i <= mid)a[idx++] = arr[i++];
-       while(j <= hi)a[idx++] = arr[j++];
-
-       for(int i = 0 ; i < hi - lo + 1 ; i++)
-       {
-           arr[i + lo] = a[i];
-       }
-
-       delete []a;
-    }
-
-    void compare(vector<int>&arr,int lo,int mid,int hi)
-    {
-        int curr = mid + 1;
-
-        for(int i = lo ; i <= mid ; i++)
+        while(i <= mid && j <= hi)
         {
-            while(curr <= hi && arr[i] > (long long)2*arr[curr])
+            if(arr[i].first > arr[j].first)
             {
-                curr++;
+                temp[index] = arr[j];
+                j = j + 1;
             }
+            else
+            {
+                temp[index] = arr[i];
+                i = i + 1;
+            }
+            index = index + 1;
+        }
 
-            pairs += (curr - mid - 1);
+        while(i <= mid)
+        {
+            temp[index++] = arr[i++];
+        }
+
+        while(j <= hi)
+        {
+            temp[index++] = arr[j++];
+        }
+
+        for(i = lo ; i <= hi ; i++)
+        {
+            arr[i] = temp[i - lo];
         }
     }
 
-    void mergeSort(vector<int>&arr,int lo,int hi)
+    void countInversions(vector<pair<int,int>>&arr,int lo,int mid,int hi,int &counter)
+    {
+        int total_count = 0;
+        int j = mid + 1;
+
+        for(int i = lo ; i <= mid ; i++)
+        {
+            while(j <= hi && (long long)arr[i].first > (long long)2*(long long)arr[j].first)
+            {
+                total_count++;
+                j = j + 1;
+            }
+            counter += total_count;
+        }
+    }
+
+    void mergeSort(vector<pair<int,int>>&arr,int lo,int hi,int &counter)
     {
         if(lo >= hi)return;
 
         int mid = (lo + hi)>>1;
-        mergeSort(arr,lo,mid);
-        mergeSort(arr,mid+1,hi);
 
-        compare(arr,lo,mid,hi);
+        mergeSort(arr,lo,mid,counter);
+        mergeSort(arr,mid+1,hi,counter);
+
+        countInversions(arr,lo,mid,hi,counter);
         mergeArrays(arr,lo,mid,hi);
     }
 
 public:
     int reversePairs(vector<int>& nums) 
     {
-        pairs = 0;  
+        vector<pair<int,int>>arr;
+        int n = nums.size();
 
-        mergeSort(nums,0,nums.size()-1);
+        for(int i = 0 ; i < n ; i++)
+        {
+            arr.push_back({nums[i],i});
+        }   
 
-        return pairs;  
+        int ans = 0;
+        mergeSort(arr,0,n-1,ans);
+        return ans;
     }
 };
