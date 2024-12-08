ab9903b9c54ca173d7d703d632e55d50902a5616
VVSD Charan <105978561+VVSD-Charan@users.noreply.github.com>
2024-12-08 23:40:45 +0530

    Time: 515 ms (50.00%) | Memory: 41.3 MB (38.46%) - LeetSync

---
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    def maxTwoEvents(self, events):
+        """
+        :type events: List[List[int]]
+        :rtype: int
+        """
+
+        # Sorting events based on starting times 
+        events.sort()
+
+        # Creating a list for suffix_maximum
+        n = len(events)
+        suffix_maximum = [events[n-1][2]]
+        for i in range(n-2,-1,-1):
+            suffix_maximum.append(max(events[i][2],suffix_maximum[-1]))
+        suffix_maximum = suffix_maximum[::-1]
+
+        #Iterating intervals
+        maximum_sum = suffix_maximum[0]
+        for i in range(n-1):
+            low = i+1
+            high = n-1
+            target = events[i][1]+1
+
+            while high-low > 1:
+                mid = (low + high)//2
+                if events[mid][0] >= target:
+                    high = mid
+                else:
+                    low = mid
+
+            if events[low][0] >= target:
+                maximum_sum = max(maximum_sum,events[i][2]+suffix_maximum[low])
+            elif events[high][0] >= target:
+                maximum_sum = max(maximum_sum,events[i][2] + suffix_maximum[high])
+
+        return maximum_sum
+
+        
+        
