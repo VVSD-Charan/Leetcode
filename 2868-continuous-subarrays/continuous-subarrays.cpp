67ae918e5c927df8aa5ba31c42ffa57f1cd25034
VVSD Charan <105978561+VVSD-Charan@users.noreply.github.com>
2024-12-14 21:36:17 +0530

    Time: 83 ms (91.32%) | Memory: 113.2 MB (75.63%) - LeetSync

--- 2868-continuous-subarrays/continuous-subarrays.cpp
+++ 2868-continuous-subarrays/continuous-subarrays.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
     long long continuousSubarrays(vector<int>& nums) {
+        map<int,int>mp;
+        long long low = 0;
+        long long high = 0;
         long long total = 0;
-        multiset<int>ms;
-
-        int low = 0;
-        int high = 0;
 
         while(high < nums.size())
         {
-           ms.insert(nums[high]);
+            mp[nums[high]]++;
 
-           auto mini = ms.begin();
-           auto maxi = ms.end();    maxi--;
+            auto mini = mp.begin();
+            auto maxi = mp.end();   maxi--;
 
-           while(*maxi-*mini > 2)
-           {
-                ms.erase(ms.find(nums[low]));
-                low++;
+            while((*maxi).first - (*mini).first > 2)
+            {
+                mp[nums[low]]--;
+                if(mp[nums[low]] == 0){
+                    mp.erase(mp.find(nums[low]));
+                }
+                low = low + 1;
 
-                maxi = ms.end();    maxi--;
-                mini = ms.begin();
-           }
+                mini = mp.begin();
+                maxi = mp.end();    maxi--;
+            }
 
-           total += (high-low+1);
-           high++;
+            total += (high-low+1);
+            high = high + 1;
         }
 
         return total;
